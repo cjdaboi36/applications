0cf203af2dd071208eec7528796f3293901e7bcf
cj_daboi36 <136152652+cjdaboi36@users.noreply.github.com>
2025-04-24 18:30:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ app = Sanic("ModerationServer")
 # Change this to the actual folder containing the files
 MODERATION_FOLDER = os.path.abspath("modapps")
 app.static('/static', './static')
+@app.route("/")
+async def serve_moderation_file(request, filename):
+    file_path = os.path.join(MODERATION_FOLDER, filename)
 
+    if not os.path.isfile(file_path):
+        return response.text("File not found", status=404)
+
+    return await response.file(file_path)
 @app.route("/moderation/<filename>")
 async def serve_moderation_file(request, filename):
     file_path = os.path.join(MODERATION_FOLDER, filename)
